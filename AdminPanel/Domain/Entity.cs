5a6dd4e122659fdb02e9cd3f1e13e15f7cbc9966
DinaraZZ <dinara.rakhymbekova@gmail.com>
2019-12-11 21:42:15 +0600

    Add project files.

---
+++ AdminPanel/Domain/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+    }
+}
---
+++ AdminPanel/ManagerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AdminPanel.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel
+{
+    public class ManagerDbContext : DbContext
+    {
+        public ManagerDbContext() : base("Server=A-104-4;Database=Products;Trusted_Connection=true;")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Product> Products { get; set; }
+    }
+}
